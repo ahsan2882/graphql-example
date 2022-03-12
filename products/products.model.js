43e60797afeaf3e3f733b949b86eaeea6940f385
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-12 14:55:41 +0500

    Update products.model.js

--- products/products.model.js
+++ products/products.model.js
@@ -13,7 +13,8 @@ const products = [
     {
         id: 'bluejeans',
         description: 'Blue Jeans',
-        price: 55.55
+        price: 55.55,
+        reviews: []
     }
 ];
 
@@ -21,6 +22,44 @@ function getAllProducts() {
     return products;
 }
 
+function getProductsByPrice(min, max) {
+    return [...products.filter((product) => {
+        return product.price >= min && product.price <= max;
+    })]
+}
+
+function getProductById(id) {
+    return products.find((product) => {
+        return product.id === id;
+    });
+}
+
+function addNewProduct(id, description, price) {
+    const newProd = {
+        id,
+        description,
+        price,
+        reviews: []
+    };
+    products.push(newProd);
+    return newProd;
+}
+
+function addProductReview(id, rating, comment) {
+    const prod = products.find((product) => {
+        return product.id === id;
+    });
+    prod.reviews.push({
+        rating,
+        comment
+    });
+    return prod;
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductsByPrice,
+    getProductById,
+    addNewProduct,
+    addProductReview
 }
