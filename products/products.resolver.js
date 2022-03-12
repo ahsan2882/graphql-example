9de6da54e3629ccad684e9cd5640699db7e57329
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-12 14:55:44 +0500

    Update products.resolver.js

--- products/products.resolver.js
+++ products/products.resolver.js
@@ -1,9 +1,23 @@
-const { getAllProducts } = require("./products.model");
+const { getAllProducts, getProductsByPrice, getProductById, addNewProduct, addProductReview } = require("./products.model");
 
 module.exports = {
     Query: {
         products: () => {
             return getAllProducts();
+        },
+        productsByPrice: (_, args) => {
+            return getProductsByPrice(args.min, args.max)
+        },
+        product: (_, args) => {
+            return getProductById(args.id)
+        }
+    },
+    Mutation: {
+        addNewProduct: (_, args) => {
+            return addNewProduct(args.id, args.description, args.price);
+        },
+        addProductReview: (_, args) => {
+            return addProductReview(args.id, args.rating, args.comment);
         }
     }
 }
